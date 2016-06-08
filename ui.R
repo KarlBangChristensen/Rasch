998efae0fc13d023fc08295be4b6f4009fd6d82e
KarlBangChristensen <KACH@sund.ku.dk>
2016-06-08 14:38:46 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -6,6 +6,33 @@ shinyUI(pageWithSidebar(
   headerPanel("Rasch model"),
 
   sidebarPanel(
+  
+  shinyUI(fluidPage(
+  titlePanel("Uploading Files"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput('file1', 'Choose CSV File',
+                accept=c('text/csv', 
+								 'text/comma-separated-values,text/plain', 
+								 '.csv')),
+      tags$hr(),
+      checkboxInput('header', 'Header', TRUE),
+      radioButtons('sep', 'Separator',
+                   c(Comma=',',
+                     Semicolon=';',
+                     Tab='\t'),
+                   ','),
+      radioButtons('quote', 'Quote',
+                   c(None='',
+                     'Double Quote'='"',
+                     'Single Quote'="'"),
+                   '"')
+    ),
+    mainPanel(
+      tableOutput('contents')
+    )
+  )
+))
     
     radioButtons("Modeltype", "Model type:",
                  list("Dichtomous Rasch model" = "RM",
