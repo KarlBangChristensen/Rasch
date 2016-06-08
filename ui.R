4bee08183bcf631556a003231f50c56daf847bbf
KarlBangChristensen <KACH@sund.ku.dk>
2016-06-08 14:41:54 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,19 +2,13 @@ library(shiny)
 
 shinyUI(pageWithSidebar(
   
-  # Application title
   headerPanel("Rasch model"),
 
-  sidebarPanel(
-  
-  shinyUI(fluidPage(
-  titlePanel("Uploading Files"),
-  sidebarLayout(
     sidebarPanel(
       fileInput('file1', 'Choose CSV File',
                 accept=c('text/csv', 
-								 'text/comma-separated-values,text/plain', 
-								 '.csv')),
+                         'text/comma-separated-values,text/plain',
+                         '.csv')),
       tags$hr(),
       checkboxInput('header', 'Header', TRUE),
       radioButtons('sep', 'Separator',
